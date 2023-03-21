248a502b4c493a668305ed1b7b500aa5cc08c93e
hir-yk <84175137+hir-yk@users.noreply.github.com>
2023-03-21 16:39:32 +0900

    add files

---
+++ turn_offset_controller/turn_offset_controller.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+from autoware_auto_vehicle_msgs.msg import TurnIndicatorsReport
+from tier4_planning_msgs.msg import LateralOffset
+
+class TurnOffsetController(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            TurnIndicatorsReport,
+            '/vehicle/status/turn_indicators_status',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+        self.publisher_ = self.create_publisher(LateralOffset, '/planning/scenario_planning/lane_driving/behavior_planning/behavior_path_planner/input/lateral_offset', 10)
+
+    def listener_callback(self, msg):
+        self.get_logger().info('I heard: "%d"' % msg.report)
+
+        offset_msg = LateralOffset()
+
+        if msg.report == 1:
+            offset_msg.lateral_offset = 0.0
+        elif msg.report == 2:
+            offset_msg.lateral_offset = 0.5
+        elif msg.report == 3:
+            offset_msg.lateral_offset = -0.5
+
+        self.publisher_.publish(offset_msg)
+        self.get_logger().info('turn_signal_report: "%d"' % msg.report)
+        self.get_logger().info('publishing_offset: "%f"' % offset_msg.lateral_offset)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    my_subscriber = TurnOffsetController()
+
+    rclpy.spin(my_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    my_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
